1ef07e0eeb795e3d8ef968b25b82bce51bc7b937
aserghine6223 <aserghine6223@gmail.com>
2026-02-09 11:21:47 -0800

    Update Team.java

--- src/Team.java
+++ src/Team.java
@@ -1,2 +1,35 @@
 public class Team {
+  private String gender;
+    private ArrayList<Player> roster;
+    private String sport;
+
+    public Team (String a, String c){
+        gender = a;
+        roster = new ArrayList<>();
+        sport = c;
+    }
+
+    public void addPlayer(Player p) {
+        roster.add(p);
+    }
+
+    public String getGender(){
+        return gender;
+    }
+    public ArrayList<Player> getRoster(){
+        return roster;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setRoster(ArrayList<Player> roster) {
+        this.roster = roster;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
 }
+
